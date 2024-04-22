0eb8ee4e8af940e4ce4ccb9cabb6e26766d1d7dc
AlvaroWau <alvarobajo893@gmail.com>
2024-04-22 13:35:22 +0200

    mas ejercicios

--- JAVA/08_ejercicio/src/main/java/org/alvarowau/Main.java
+++ JAVA/08_ejercicio/src/main/java/org/alvarowau/Main.java
@@ -1,7 +1,59 @@
 package org.alvarowau;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+        double precioEntrada = 8;
+        double precioTotalEntradas;
+        boolean tarjeta = false;
+
+        System.out.print("Digite el número de entradas: ");
+        int entradas = sc.nextInt();
+
+        System.out.println("Digite el día de la semana:");
+        System.out.println("l. Lunes");
+        System.out.println("m. Martes");
+        System.out.println("x. Miércoles");
+        System.out.println("j. Jueves");
+        System.out.println("v. Viernes");
+        System.out.println("s. Sábado");
+        System.out.println("d. Domingo");
+        String dia = sc.next();
+
+        System.out.print("¿Tiene la tarjeta del cine? 'n. NO s. SI': ");
+        String tiene = sc.next();
+
+        switch (tiene) {
+            case "n":
+                tarjeta = false;
+                break;
+            case "s":
+                tarjeta = true;
+                break;
+            default:
+                tarjeta = false;
+        }
+        sc.close();
+
+        switch (dia) {
+            case "x":
+                precioEntrada = 5;
+                precioTotalEntradas = precioEntrada * entradas;
+                break;
+            case "j":
+                int aux = entradas % 2;
+                entradas /= 2;
+                precioTotalEntradas = (entradas * 11) + (aux * precioEntrada);
+                break;
+            default:
+                precioTotalEntradas = precioEntrada * entradas;
+        }
+        if(tarjeta) {
+            precioTotalEntradas -= (precioTotalEntradas * 0.10);
+        }
+
+        System.out.println("El precio total de las entradas es: " + precioTotalEntradas);
     }
 }
---
+++ JAVA/09_ejercicio/enunciado.md
@@ -0,0 +1,46 @@
+# Ejercicio: Juego Piedra - Papel - Tijeras - Lagarto - Spock
+
+## Descripción
+
+En este ejercicio, desarrollaremos un programa que simule el juego "Piedra - Papel - Tijeras - Lagarto - Spock". Este juego es una extensión del clásico "Piedra - Papel - Tijeras" e incluye dos opciones adicionales: "Lagarto" y "Spock".
+
+## Enunciado
+
+Escribe un programa que permita a dos jugadores enfrentarse en una partida de "Piedra - Papel - Tijeras - Lagarto - Spock". El programa debe solicitar a cada jugador que elija una de las siguientes opciones:
+
+- Piedra
+- Papel
+- Tijeras
+- Lagarto
+- Spock
+
+Basado en las reglas del juego, determina cuál de los dos jugadores es el ganador o si la partida termina en empate.
+
+## Reglas del Juego
+
+Las reglas para determinar al ganador son las siguientes:
+
+- Piedra aplasta tijeras.
+- Tijeras cortan papel.
+- Papel cubre piedra.
+- Piedra aplasta lagarto.
+- Lagarto envenena Spock.
+- Spock destroza tijeras.
+- Tijeras decapitan lagarto.
+- Lagarto come papel.
+- Papel refuta Spock.
+- Spock vaporiza piedra.
+
+Si ambos jugadores eligen la misma opción, el juego termina en empate.
+
+## Ejemplo
+
+- Jugador 1 elige "Piedra" y Jugador 2 elige "Tijeras". El ganador es Jugador 1, ya que la piedra aplasta las tijeras.
+
+## Objetivos de Aprendizaje
+
+- Practicar el uso de condicionales y comparaciones en un programa.
+- Implementar algoritmos para la toma de decisiones basados en reglas específicas.
+- Mejorar la comprensión sobre la lógica de programación y la resolución de problemas.
+
+¡Anímate a resolver este ejercicio para mejorar tus habilidades de programación!
