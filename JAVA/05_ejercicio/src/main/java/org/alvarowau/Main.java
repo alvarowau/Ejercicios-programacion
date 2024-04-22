3eab9ff13c8d9471bc513a640bc36c4855f8b541
AlvaroWau <alvarobajo893@gmail.com>
2024-04-22 11:28:50 +0200

    ejercicio 5

--- JAVA/05_ejercicio/src/main/java/org/alvarowau/Main.java
+++ JAVA/05_ejercicio/src/main/java/org/alvarowau/Main.java
@@ -1,7 +1,28 @@
 package org.alvarowau;
 
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.print("Digite la primera nota: ");
+        float primera = entrada.nextFloat();
+        System.out.print("Digite la segunda nota: ");
+        float segunda = entrada.nextFloat();
+        System.out.print("Digite la tercera nota: ");
+        float tercera = entrada.nextFloat();
+        entrada.close();
+
+        float resultado = (primera + segunda + tercera) / 3;
+        DecimalFormat df = new DecimalFormat("#.##");
+        System.out.println("La nota media es: " + df.format(resultado));
+        if(resultado >= 5){
+            System.out.println("El alumno está aprobado");
+        }else{
+            System.out.println("El alumno no está aprobado");
+        }
+
     }
 }
